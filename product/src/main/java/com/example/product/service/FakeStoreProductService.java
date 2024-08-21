54318c026a9bf745526ed5101ab08d4337fbc920
Ramesh Rs <rameshrs@centizen.com>
2024-08-21 15:54:33 +0530

    Change in service

--- product/src/main/java/com/example/product/service/FakeStoreProductService.java
+++ product/src/main/java/com/example/product/service/FakeStoreProductService.java
@@ -2,8 +2,9 @@ package com.example.product.service;
 
 import com.example.product.dtoa.FakeStoreProductDto;
 import com.example.product.models.Product;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+@Service
 public class FakeStoreProductService implements ProductService{
     @Override
     public Product getProductById(long id){
